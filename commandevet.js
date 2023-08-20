0a4ecba67caadde351e46125613d2f02db398a2a
SANDRINE <marieangeleanoh@gmail.com>
2023-08-20 00:24:45 +0000

    site web sandy

---
+++ commandevet.js
@@ -0,0 +1,14 @@
+var nombre, commande, vetement;
+
+nombre = 100;
+    var commande = prompt('Saisir le nombre de vetements')
+       
+    if(commande<=100)
+        {
+        alert("Vetements disponible");
+        }
+
+    else{
+        alert("Nombre de vetements indisponible");
+        }
+        
